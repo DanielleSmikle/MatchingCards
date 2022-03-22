0551087bdf850bfa1eb323bfaf9aa3a632bdcccf
Username <daniellesmikle@Danielles-Air.home>
2022-03-21 20:11:05 -0400

    all cards are loaded

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const card12 = document.getElementById('card12')
 
 let choice1, choice3=''
 let choice2, choice4=''
+let choice5, choice6=''
+let choice7, choice8=''
+let choice9, choice10=''
+let choice11, choice12=''
 let points= 0
 
 card1.addEventListener("click",function(event){
@@ -33,6 +37,8 @@ card1.addEventListener("click",function(event){
     card1.firstElementChild.classList.toggle("hidden")
     card1.lastElementChild.classList.toggle("hidden")
 })
+
+
 card2.addEventListener("click",function(event){
     card2.firstElementChild.classList.toggle("hidden")
     card2.lastElementChild.classList.toggle("hidden")
@@ -91,36 +97,147 @@ console.log(points)
 })
 
 
-
 card5.addEventListener("click",function(event){
+    if (choice5 ==''){
+        choice5='julius'
+    } else {
+    if (choice6 ==''){
+        choice6= 'julius'
+
+    }
+
+}
+    if (choice5 == choice6){
+        points+=2
+
+    }
+   
     card5.firstElementChild.classList.toggle("hidden")
     card5.lastElementChild.classList.toggle("hidden")
 })
+
+
+
+
+
 card6.addEventListener("click",function(event){
     card6.firstElementChild.classList.toggle("hidden")
     card6.lastElementChild.classList.toggle("hidden")
+
+    if (choice5 ==''){
+        choice5='julius'
+    } else {
+    if (choice6 ==''){
+        choice6= 'julius'
+    }
+}
+    if (choice5 == choice6){
+    points+=2
+
+}
+console.log(points)
 })
+
 card7.addEventListener("click",function(event){
+    if (choice7 ==''){
+        choice7='pat'
+    } else {
+    if (choice8 ==''){
+        choice8= 'pat'
+
+    }
+
+}
+    if (choice7 == choice8){
+        points+=2
+
+    }
     card7.firstElementChild.classList.toggle("hidden")
     card7.lastElementChild.classList.toggle("hidden")
 })
 card8.addEventListener("click",function(event){
     card8.firstElementChild.classList.toggle("hidden")
     card8.lastElementChild.classList.toggle("hidden")
+
+    if (choice7 ==''){
+        choice7='pat'
+    } else {
+    if (choice8 ==''){
+        choice8= 'pat'
+    }
+}
+    if (choice7 == choice8){
+    points+=2
+
+}
+console.log(points)
 })
+
 card9.addEventListener("click",function(event){
+    if (choice9 ==''){
+        choice9='meme'
+    } else {
+    if (choice10 ==''){
+        choice10= 'meme'
+
+    }
+
+}
+    if (choice9 == choice10){
+        points+=2
+
+    }
     card9.firstElementChild.classList.toggle("hidden")
     card9.lastElementChild.classList.toggle("hidden")
 })
 card10.addEventListener("click",function(event){
     card10.firstElementChild.classList.toggle("hidden")
     card10.lastElementChild.classList.toggle("hidden")
+    if (choice9 ==''){
+        choice9='meme'
+    } else {
+    if (choice10 ==''){
+        choice10= 'meme'
+    }
+}
+    if (choice9 == choice10){
+    points+=2
+
+}
+console.log(points)
 })
+
 card11.addEventListener("click",function(event){
+    if (choice11 ==''){
+        choice11='snoop'
+    } else {
+    if (choice12 ==''){
+        choice12= 'snoop'
+
+    }
+
+}
+    if (choice11 == choice12){
+        points+=2
+
+    }
     card11.firstElementChild.classList.toggle("hidden")
     card11.lastElementChild.classList.toggle("hidden")
 })
 card12.addEventListener("click",function(event){
     card12.firstElementChild.classList.toggle("hidden")
     card12.lastElementChild.classList.toggle("hidden")
+    if (choice11 ==''){
+        choice11='snoop'
+    } else {
+    if (choice12 ==''){
+        choice12= 'snoop'
+    }
+}
+    if (choice11 == choice12){
+    points+=2
+
+}
+console.log(points)
 })
+
